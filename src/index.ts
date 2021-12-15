091edafd2297712b9ac4c72b561ebf113e8b4560
pvinig <83087064+pvinig@users.noreply.github.com>
2021-12-14 23:26:59 -0300

    crawler das questões do DETRAN, referente a prova teórica

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,61 @@
-import fastify from "fastify";
-import cheerio from "cheerio";
+/** @format */
 
-const json = require("./detran.json");
+import fastify from 'fastify';
+import cheerio from 'cheerio';
+
+const json = require('./detran.json');
 
 const app = fastify({ logger: true });
 
-app.get("/", async (req, res) => {
-  const response = {
-    questions: [] as any,
-  };
-  const $ = cheerio.load(json.content);
+app.get('/', async (req, res) => {
+	const response = {
+		questions: [] as any,
+	};
+	const $ = cheerio.load(json.content);
 
-  $(".wpProQuiz_listItem").each(function (index, element) {
-    const title = $(element).find(".wpProQuiz_question_text").text().trim();
+	$('.wpProQuiz_listItem').each(function (index, element) {
+		const title = $(element).find('.wpProQuiz_question_text').text().trim();
 
-    console.log("title", title);
+		let alternatives = $(element)
+			.find('.wpProQuiz_questionList')
+			.text()
+			.split('  ');
+		alternatives = alternatives.map((option) => {
+			let validOption = [];
+			if (option.includes('.')) validOption.push(option);
+			return validOption.toString().replace('.', '').trim();
+		});
+		alternatives = alternatives.filter((notNullQuestion: any) => {
+			return notNullQuestion;
+		});
 
-    response.questions.push({
-      title,
-    });
-  });
+		let questionId: any = $(element)
+			.find('.wpProQuiz_questionList')
+			.data('question_id');
+		let correctOption = json.json[questionId].correct;
+		correctOption = correctOption.indexOf(1) + 1;
 
-  return response;
+		console.log('title', title);
+		console.log('alternative', alternatives);
+		console.log('correctOption', correctOption);
+
+		response.questions.push({
+			title,
+			alternatives,
+			correctOption,
+		});
+	});
+
+	return response;
 });
 
 const start = async () => {
-  try {
-    await app.listen(3000);
-  } catch (err) {
-    app.log.error(err);
-    process.exit(1);
-  }
+	try {
+		await app.listen(3000);
+	} catch (err) {
+		app.log.error(err);
+		process.exit(1);
+	}
 };
 
 start();
